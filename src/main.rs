6e5061f1b6efdf466893b0a12862ed1f7bbe9225
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-11-13 22:06:47 -0800

    Add simple ray casting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+mod ray;
+
+use crate::ray::Ray;
 use clap::Parser;
 use image::RgbImage;
 use indicatif::ParallelProgressIterator;
-use nalgebra::Vector3;
+use nalgebra::{vector, Vector3};
 use rayon::prelude::*;
 use std::path::PathBuf;
+
+fn ray_colour(r: &Ray) -> Vector3<f64> {
+    let unit_direction = r.direction.normalize();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * vector![1.0, 1.0, 1.0] + t * vector![0.5, 0.7, 1.0]
+}
 
 fn vector_to_rgb(colour: &Vector3<f64>) -> [u8; 3] {
     [
@@ -23,8 +32,21 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let image_width: u32 = 255;
-    let image_height: u32 = 255;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f64) / aspect_ratio) as u32;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = vector![0.0, 0.0, 0.0];
+    let horizontal = vector![viewport_width, 0.0, 0.0];
+    let vertical = vector![0.0, viewport_height, 0.0];
+    let upper_left_corner =
+        origin - horizontal / 2.0 + vertical / 2.0 - vector![0.0, 0.0, focal_length];
 
     let buffer: Vec<u8> = (0..image_height)
         .flat_map(|j| (0..image_width).map(move |i| (i, j)))
@@ -32,12 +54,14 @@ fn main() {
         .into_par_iter()
         .progress()
         .flat_map(|(x, y)| {
-            let r = (x as f64) / (image_width - 1) as f64;
-            let g = (image_height - y) as f64 / (image_height - 1) as f64;
-            let b = 0.25;
-            let colour = Vector3::new(r, g, b);
-
-            vector_to_rgb(&colour)
+            let u = (x as f64) / (image_width - 1) as f64;
+            let v = (y as f64) / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                upper_left_corner + u * horizontal - v * vertical - origin,
+            );
+            let pixel_colour = ray_colour(&r);
+            vector_to_rgb(&pixel_colour)
         })
         .collect();
     let img = RgbImage::from_raw(image_width, image_height, buffer).unwrap();
---
+++ src/ray.rs
@@ -0,0 +1,17 @@
+use nalgebra::Vector3;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Ray {
+    pub origin: Vector3<f64>,
+    pub direction: Vector3<f64>,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3<f64>, direction: Vector3<f64>) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Vector3<f64> {
+        self.origin + t * self.direction
+    }
+}
